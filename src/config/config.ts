1cbe7a3848ca58d5cd4ef3b637dc67ee6660375d
Gabini - Mac <haile.mak11@gmail.com>
2022-08-07 14:27:39 +0300

    initial commit,added .env file,completed todos

---
+++ src/config/config.ts
@@ -0,0 +1,22 @@
+export const config = {
+  "dev": {
+    "username": "master",
+    "password": "password",
+    "database": "udagramdatabase",
+    "host": "udagramdatabaseinstance-1.cdrsize5pznl.us-east-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "us-east-2",
+    "aws_profile": "default",
+    "aws_media_bucket": "udagram-ruttner-dev"
+  },
+  "jwt": {
+    "secret": " "
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
